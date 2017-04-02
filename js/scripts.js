4e418dada15e2fc83a948138baf61775dfc9ec3b
ENTER <ENTER>
2017-04-02 14:07:48 +0200

    8.9 Prosze o komentarz bo zrobilem to zadanie z pomoca i za bardzo nie rozumiem co i dlaczego powstalo

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function rysujChoinke(n) {
+	for ( var k = 1 ; k < n; k++) {
+		var star = '*';
+			for ( var j = 1 ; j < k*2-1; j++) {
+				star  += '*';
+			}
+			console.log(star);
+	}
+	
+}
+
+rysujChoinke(6);
+//zrobiłem to z dużą pomocą - szczerze to wogóle nie rozumiem tego zadania - prosiłbym o krótki komentarz dlaczego tj. tak a nie inaczej.
